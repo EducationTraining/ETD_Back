35fb364593066a91e7ec2d239d841b5d8de18ca0
yeiamx <925166430@qq.com>
2019-12-08 15:08:53 +0800

    build frame

--- src/main/java/com/etd/etdservice/EtdServiceApplication.java
+++ src/main/java/com/etd/etdservice/EtdServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class EtdServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EtdServiceApplication.class, args);
